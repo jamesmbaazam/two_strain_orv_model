01fcf0e12c3420e8cf4e436bd1c600fd1e7de2d3
James Mba Azam <jamesazam@sun.ac.za>
2021-07-15 13:54:28 +0200

    Run the mode with and without vaccination

--- scripts/two_strain_model.R
+++ scripts/two_strain_model.R
@@ -13,7 +13,7 @@ two_strain_model <- function(t, y, parms){
     with(c(as.list(y),parms),{
         
         beta_w <- R0_w*gamma_w
-        beta_m <- ifelse(t < 25, beta_w, R0_m*gamma_m)
+        beta_m <- ifelse(t < variant_emergence_time, 0, R0_m*gamma_m)
         
         
         dSdt <- - beta_w*S - beta_m*S - epsilon*S + alpha_w*R_w + alpha_m*R_m
@@ -32,26 +32,37 @@ two_strain_model <- function(t, y, parms){
 
 #Running the model
 library(deSolve)
-library(ggplot2)
-library(tibble)
+library(tidyverse)
+
 
 ### INITIALIZE PARAMETER SETTINGS
 
-parms <- c(R0_w = 1, 
+parms_no_vax_model <- c(R0_w = 1, 
            R0_m = 2,
            gamma_w = 1/8,
            gamma_m = 1/8,
-           epsilon = 1/45, 
-           alpha_w = 1/360,
-           alpha_m = 1/360
-           )    
+           epsilon = 0, 
+           alpha_w = 0,
+           alpha_m = 0,
+           variant_emergence_time = 50
+           ) 
 
-dt    <- seq(0, 100, 1)      # set the time points for evaluation
+parms_vax_model <- c(R0_w = 1, 
+                  R0_m = 2,
+                  gamma_w = 1/8,
+                  gamma_m = 1/8,
+                  epsilon = 1/14, 
+                  alpha_w = 0,
+                  alpha_m = 0,
+                  variant_emergence_time = 50
+)    
+
+dt <- seq(0, 365, 1)      # set the time points for evaluation
 
 
 # Initial conditions
 
-inits <- c(S = 1000, 
+inits <- c(S = 999, 
            I_w = 1, 
            I_m = 0, 
            R_w = 0, 
@@ -67,31 +78,32 @@ N
 
 ## Use lsoda to solve the differential equations numerically. 
 
-out <- as_tibble(lsoda(inits, dt, two_strain_model, parms = parms)) 
+no_vax_dynamics <- as.data.frame(lsoda(inits, dt, two_strain_model, parms = parms_no_vax_model)) 
+vax_dynamics <- no_vax_dynamics <- as.data.frame(lsoda(inits, dt, two_strain_model, parms = parms_vax_model)) 
 
-
-two_strain_model_plot <- ggplot(data = out) + 
+#complete dynamics
+sirv_plot <- ggplot(data = no_vax_dynamics) + 
     geom_line(aes(x = time,
                   y = S,
                   color = 'S'
                   ),
               size = 1.2
               ) +
-    geom_line(data = out, 
+    geom_line(data = no_vax_dynamics, 
               aes(x = time, 
                   y = I_w,
                   color = 'I_w'
               ), 
               size = 1.2
     ) +
-    geom_line(data = out, 
+    geom_line(data = no_vax_dynamics, 
               aes(x = time, 
                   y = I_m,
                   color = 'I_m'
               ), 
               size = 1.2
     ) +
-    geom_line(data = out, 
+    geom_line(data = no_vax_dynamics, 
               aes(x = time, 
                   y = V,
                   color = 'V'
@@ -104,4 +116,31 @@ two_strain_model_plot <- ggplot(data = out) +
                                   'V' = 'purple'
                                   ))
     
-print(two_strain_model_plot)
+print(sirv_plot)
+
+
+
+#' cumulative incidence
+no_vax_dynamics_with_cum_incidence <- no_vax_dynamics %>% mutate(I = I_w + I_m, 
+                                   I_cum_no_vax = cumsum(I)
+                                   ) 
+
+vax_dynamics_with_cum_incidence <- vax_dynamics %>% mutate(I = I_w + I_m, 
+                                                           I_cum_vax = cumsum(I)
+                                                           ) 
+    
+
+cum_inc_plot <- ggplot(data = no_vax_dynamics_with_cum_incidence) + 
+    geom_line(aes(x = time,
+                  y = I_cum_no_vax
+                  ),
+              color = 'red', 
+              size = 1.2
+              ) +
+    geom_line(data = vax_dynamics_with_cum_incidence, 
+              aes(x = time, 
+                  y = I_cum_vax
+                  )
+              )
+    
+print(cum_inc_plot)
