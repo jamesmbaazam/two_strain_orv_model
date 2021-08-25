f598d21b1385b33c5cfeda02bff05872cafa8177
James Azam <jamesazam@sun.ac.za>
2021-08-25 23:38:35 +0200

    recoded the model more succinctly

--- scripts/two_strain_model.R
+++ scripts/two_strain_model.R
@@ -1,40 +1,38 @@
 #' two strain model A model of two co-existing strains of the same virus
 #'
-#' @param t 
-#' @param y 
-#' @param parms 
+#' @param t
+#' @param y
+#' @param parms
 #'
 #' @return
 #' @export
 #'
 #' @examples
-two_strain_model <- function(t, y, parms){
-    
-    with(c(as.list(y),parms),{
-        
-        #Introduce the index variant case after t=variant_emergence_day
-        beta_m <- ifelse(t < variant_emergence_day, 0, beta_m)
-        
-        #implement NPIs with intensity phi between a time period
-        phi <- ifelse(t < npi_implementation_day | t > npi_implementation_day + npi_duration, 
-                                0, 
-                                phi
-                                )
-        
-        epsilon <- ifelse(t < vax_day | t > vax_day + campaign_duration, 0, epsilon)
-        
-        dSdt = - (1 -  phi)*beta_w*S*Iw - (1 - phi)*beta_m*S*Im - epsilon*S  
-        dIwdt = (1 - phi)*beta_w*S*Iw - gamma_w*Iw  
-        dImdt = (1 -  phi)*beta_m*S*Im - gamma_m*Im  
-        dIwmdt = (1 - phi)*(1 - sigma_w)*beta_m*RwSm*Im - gamma_m*Iwm  
-        dImwdt = (1 - phi)*(1 - sigma_m)*beta_w*RmSw*Im - gamma_w*Imw  
-        dRwSmdt = gamma_w*Iw - epsilon*RwSm - (1 - phi)*(1 - sigma_w)*beta_m*RwSm*Im 
-        dRmSwdt = gamma_m*Im - epsilon*RmSw - (1 - phi)*(1 - sigma_w)*beta_w*RmSw*Iw 
-        dRdt = gamma_m*Iwm + gamma_w*Imw  
-        dVdt =   epsilon*(S +  RwSm + RmSw)
-        
-        mod_result <- list(c(dSdt, dIwdt, dImdt, dIwmdt, dImwdt, dRwSmdt, dRmSwdt, dRdt, dVdt))
-        return(mod_result)
-    })
-}
+two_strain_model <- function(t, y, parms) {
+  with(c(as.list(y), parms), {
 
+    # Introduce the index variant case after t=variant_emergence_day
+    beta_m <- ifelse(t < variant_emergence_day, 0, beta_m)
+
+    # implement NPIs with intensity phi between a time period
+    phi <- ifelse(t < npi_implementation_day | t > npi_implementation_day + npi_duration, 0, phi)
+
+    FOI_Iw <- beta_w * (Iw + Imw)
+    FOI_Im <- beta_m * (Im + Iwm)
+
+    epsilon <- ifelse(t < vax_day | t > vax_day + campaign_duration, 0, epsilon)
+
+    dSdt <- -(1 - phi) * FOI_Iw * S - (1 - phi) * FOI_Im * S - epsilon * S
+    dIwdt <- (1 - phi) * FOI_Iw * S - gamma_w * Iw
+    dImdt <- (1 - phi) * FOI_Im * S - gamma_m * Im
+    dIwmdt <- (1 - phi) * (1 - sigma_w) * FOI_Im * RwSm - gamma_m * Iwm
+    dImwdt <- (1 - phi) * (1 - sigma_m) * FOI_Iw * RmSw - gamma_w * Imw
+    dRwSmdt <- gamma_w * Iw - epsilon * RwSm - (1 - phi) * (1 - sigma_w) * FOI_Im * RwSm
+    dRmSwdt <- gamma_m * Im - epsilon * RmSw - (1 - phi) * (1 - sigma_m) * FOI_Iw * RmSw
+    dRdt <- gamma_m * Iwm + gamma_w * Imw
+    dVdt <- epsilon * (S + RwSm + RmSw)
+
+    mod_result <- list(c(dSdt, dIwdt, dImdt, dIwmdt, dImwdt, dRwSmdt, dRmSwdt, dRdt, dVdt))
+    return(mod_result)
+  })
+}
