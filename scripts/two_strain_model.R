cc15ef151b7e426e17a36c6c6c65d84b53df0f3f
James Azam <jamesazam@sun.ac.za>
2021-08-26 15:59:32 +0200

    Model runs but Im dynamics has a bug

--- scripts/two_strain_model.R
+++ scripts/two_strain_model.R
@@ -8,20 +8,44 @@
 #' @export
 #'
 #' @examples
-two_strain_model <- function(t, y, parms) {
-  with(c(as.list(y), parms), {
-
-    # Introduce the index variant case after t=variant_emergence_day
-    beta_m <- ifelse(t < variant_emergence_day, 0, beta_m)
-
-    # implement NPIs with intensity phi between a time period
-    phi <- ifelse(t < npi_implementation_day | t > npi_implementation_day + npi_duration, 0, phi)
-
+two_strain_model <- function(t, y, parms, browse = F) {
+  
+    if(browse) browser()
+  
+    with(c(as.list(y), parms), {
+      
+    # ========================================  
+    # Force of infection of the wild-type virus
+    # ========================================
     FOI_Iw <- beta_w * (Iw + Imw)
+    
+    # ======================================== 
+    # Introduce transmission variant transmission after t >= variant_emergence_day
+    # ======================================== 
+    beta_m <- ifelse(t < variant_emergence_day, 0, beta_m)
+    
     FOI_Im <- beta_m * (Im + Iwm)
-
-    epsilon <- ifelse(t < vax_day | t > vax_day + campaign_duration, 0, epsilon)
-
+    
+    gamma_m <- ifelse(t < variant_emergence_day, 0, gamma_m)
+    
+    # ======================================== 
+    # Implement NPIs with intensity phi between a time period
+    # ======================================== 
+    
+    phi <- ifelse(t < npi_implementation_day | t > npi_implementation_day + npi_duration, 0, phi)
+    
+    # ======================================== 
+    # Convert the vaccination coverage to a rate 
+    # ======================================== 
+    
+    epsilon <- ifelse(t < vax_day | t > vax_day + campaign_duration | vax_coverage == 0, 0, (-log(1 - vax_coverage*coverage_correction) / campaign_duration))
+    
+    
+    
+    # ======================================== 
+    # The two strain model
+    # ======================================== 
+    
     dSdt <- -(1 - phi) * FOI_Iw * S - (1 - phi) * FOI_Im * S - epsilon * S
     dIwdt <- (1 - phi) * FOI_Iw * S - gamma_w * Iw
     dImdt <- (1 - phi) * FOI_Im * S - gamma_m * Im
