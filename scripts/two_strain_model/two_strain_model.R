48632c41cd6cff218b4692307766a5ebd938b37a
James Azam <jamesazam@sun.ac.za>
2021-10-12 21:42:44 +0200

    Reverted to the linear vaccination rate implementation

--- scripts/two_strain_model/two_strain_model.R
+++ scripts/two_strain_model/two_strain_model.R
@@ -1,19 +1,8 @@
 #' two strain model A model of two co-existing strains of the same virus
-#'
-#' @param t
-#' @param y
-#' @param parms
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' 
-#' 
-
-
-
-#' The two strain model ====
+#' @param t 
+#' @param y 
+#' @param parms 
+#' @param browse 
 
 
 two_strain_model <- function(t, y, parms, browse = FALSE) {
@@ -40,10 +29,16 @@ two_strain_model <- function(t, y, parms, browse = FALSE) {
     #coverage_correction <- 0.9909 #Use this to prevent the epsilon conversion from going to infinity when coverage = 1
     
     #epsilon <- ifelse(t < vax_start | t > vax_start + campaign_duration | vax_coverage == 0, 0, (-log(1 - vax_coverage*coverage_correction) / campaign_duration))
-    vax_target_group <- S + RwSm + RmSw
-    vax_rate <- vax_prop*vax_target_group
+    vax_eligible_pop <- S + RwSm + RmSw
+    
+    vax_rate <- vax_rate*vax_eligible_pop
+    
+    epsilon <- ifelse((t >= vax_start) & (vax_eligible_pop > 0), min(vax_rate, vax_eligible_pop), 0)
+    
+    
+   # vax_campaign_objective <- vax_coverage*vax_eligible_pop
       
-    epsilon <- ifelse((t >= vax_start) & (vax_target_group > 0), min(vax_rate, vax_target_group), 0)
+   # epsilon <- ifelse((t >= vax_start) & (vax_campaign_objective > 0), min(vax_rate, vax_campaign_objective), 0)
     
     # Model equations ####
     
@@ -65,6 +60,15 @@ two_strain_model <- function(t, y, parms, browse = FALSE) {
 
 
 #' The simulation function ====
+#'
+#' @param pop_inits 
+#' @param dynamics_parms 
+#' @param control_parms 
+#' @param max_time 
+#' @param dt 
+#' @param events_table 
+#' @param return_dynamics 
+#' @param browse 
 
 simulate_model <- function(pop_inits, dynamics_parms, 
                            control_parms, max_time, 
@@ -93,12 +97,11 @@ simulate_model <- function(pop_inits, dynamics_parms,
                                     )
                               )
   
-  total_cases <- max(sim_results$K)
-  total_vaccinated <- max(sim_results$V)
   incidence <- sim_results$Iw + sim_results$Im + sim_results$Iwm + sim_results$Imw
   peak_magnitude <- max(incidence)
+  total_cases <- max(sim_results$K)
+  total_vaccinated <- max(sim_results$V)
   
-  #' Final results time ####
   
   if(return_dynamics){
     return(sim_results)
@@ -113,8 +116,8 @@ simulate_model <- function(pop_inits, dynamics_parms,
     }
 }
 
-calc_campaign_duration <- function(vax_coverage, vax_rate, coverage_correction){
-  duration <- round(-log(1 - vax_coverage*coverage_correction) / vax_rate)
-  res <- data.frame(vax_rate = vax_rate, vax_coverage = vax_coverage, campaign_duration = duration)
-  return(res)
-}
+# calc_campaign_duration <- function(vax_coverage, vax_rate, coverage_correction){
+#   duration <- round(-log(1 - vax_coverage*coverage_correction) / vax_rate)
+#   res <- data.frame(vax_rate = vax_rate, vax_coverage = vax_coverage, campaign_duration = duration)
+#   return(res)
+# }
