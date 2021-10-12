c2cb83f2c1b49ebf3ab2538f2a603eb67cc61146
James Azam <jamesazam@sun.ac.za>
2021-10-12 21:43:59 +0200

    Set up a simulation table for 50% and 100% coverage

--- scripts/two_strain_model/sim_config_emergence_risk_adjusted.R
+++ scripts/two_strain_model/sim_config_emergence_risk_adjusted.R
@@ -35,14 +35,13 @@ npi_intensity <- 0 #Five levels of npi intensity (could correspond to the five s
 # Simple case: vaccination starts on day 1 and can achieve 100% coverage but at different rates
 vax_start <- 1
 vax_cov <- 1
-daily_vaccinated_prop <- seq(0.01E-2, 1E-2, length.out = 20) #daily proportion of total population vaccinated
+daily_vaccination_rate <- seq(0.0001, 0.01, length.out = 20) #daily proportion of total population vaccinated
 #daily_vax_rate_vec <- daily_vaccinated_prop*target_pop
 
 #Find the time it will take to achieve the assumed coverate with the given rates
-campaign_controls_df <- data.frame(vax_prop = daily_vaccinated_prop)# %>% 
-    # purrr::map_df(function(x) {
-    #     calc_campaign_duration(vax_rate = x, vax_coverage = vax_cov, coverage_correction = 0.9999)
-    # })
+campaign_controls_df <- data.frame(vax_rate = rep(daily_vaccination_rate, times = 2), 
+                                   vax_coverage = rep(c(1, 0.5), each = length(daily_vaccination_rate))
+                                   )
 
 # Full simulation table with variant emergence times appended ====
 simulation_table <- campaign_controls_df %>%
