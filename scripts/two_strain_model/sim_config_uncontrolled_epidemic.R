fb62c0c7903ed567d1c804516bbe180196a81f81
James Azam <jamesazam@sun.ac.za>
2021-10-18 13:15:14 +0200

    Move the uncontrolled epidemic control parameters df to a new script

---
+++ scripts/two_strain_model/sim_config_uncontrolled_epidemic.R
@@ -0,0 +1,14 @@
+#Packages ----
+library(deSolve)
+library(dplyr)
+
+# Helper scripts ----
+source('./scripts/two_strain_model/two_strain_model.R')
+source('./scripts/two_strain_model/sim_config_global_params.R')
+
+
+# control parameters (turned off for uncontrolled epidemic)
+no_control_parms_df <- data.frame(npi_intensity = 0, vax_coverage = 0, 
+                                  vax_start = max_time, campaign_duration = 0, 
+                                  npi_start = max_time, npi_duration = 0
+)
--- scripts/two_strain_model/simulations_uncontrolled_epidemic.R
+++ scripts/two_strain_model/simulations_uncontrolled_epidemic.R
@@ -5,12 +5,8 @@ library(dplyr)
 # Helper scripts ----
 source('./scripts/two_strain_model/two_strain_model.R')
 source('./scripts/two_strain_model/sim_config_global_params.R')
+source('./scripts/two_strain_model/sim_config_uncontrolled_epidemic.R')
 
-# control parameters (turned off for uncontrolled epidemic)
-no_control_parms_df <- data.frame(npi_intensity = 0, vax_coverage = 0, 
-                                  vax_start = max_time, campaign_duration = 0, 
-                                  npi_start = max_time, npi_duration = 0
-                                  )
 # Simulations ====
 no_control_epidemic_dynamics <- variant_emergence_times %>% 
     purrr::map_df(function(x){simulate_raw_dynamics(pop_inits = pop_inits, 
