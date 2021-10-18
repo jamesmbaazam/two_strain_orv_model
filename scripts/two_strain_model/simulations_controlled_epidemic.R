19cf5743ffdc30175c10414dc0c283ed958ed348
James Azam <jamesazam@sun.ac.za>
2021-10-18 17:04:47 +0200

    Don't run the baseline scenario separately. Also, only run for npi = 0 and 0.5

--- scripts/two_strain_model/simulations_controlled_epidemic.R
+++ scripts/two_strain_model/simulations_controlled_epidemic.R
@@ -11,28 +11,11 @@ source('./scripts/two_strain_model/sim_config_global_params.R')
 source('./scripts/two_strain_model/sim_config_emergence_risk_adjusted.R')
 
 
-# Controlled epidemic
-
-#Baseline; no variant emerges and only wild type prevails
-baseline_no_variant <- baseline_params %>% 
-    rowwise() %>% 
-    do({with(., 
-             simulate_raw_dynamics(pop_inits = pop_inits, 
-                            dynamics_parms = dynamics_params,
-                            control_parms = .,
-                            max_time = max_time, 
-                            dt = eval_times,
-                            events_table = event_df,
-                            browse = F
-             )
-    )
-    }) %>% 
-    ungroup() %>% 
-    as_tibble()  
-
+# Controlled epidemic ----
 
 #Simulations with variant emergence
-orv_full_simulation <- simulation_table %>% 
+orv_full_simulation <- orv_npi_control_config_table %>% 
+    filter(npi_intensity %in% c(0, 0.5)) %>% #npi levels of 0%, 10%, and 50%
     rowwise() %>% 
     do({with(., 
              simulate_raw_dynamics(pop_inits = pop_inits, 
