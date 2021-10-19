36b9ceecfd97ce71d856e95c36a1a0ac415613ca
James Azam <jamesazam@sun.ac.za>
2021-10-19 13:10:25 +0200

    Set up a sequential run of all the intervention scenarios

--- scripts/two_strain_model/simulations_controlled_epidemic.R
+++ scripts/two_strain_model/simulations_controlled_epidemic.R
@@ -14,6 +14,27 @@ source('./scripts/two_strain_model/sim_config_emergence_risk_adjusted.R')
 # Controlled epidemic ----
 
 #Simulations with npi and vaccination ====
+#All scenarios
+orv_npi_simulation_all_scenarios <- orv_npi_control_config_table %>% 
+    rowwise() %>% 
+    do({with(., 
+             simulate_raw_dynamics(pop_inits = pop_inits, 
+                                   dynamics_parms = dynamics_params,
+                                   control_parms = .,
+                                   max_time = max_time, 
+                                   dt = eval_times,
+                                   events_table = event_df,
+                                   get_summaries = TRUE,
+                                   browse = F
+             )
+    )
+    }) %>% 
+    ungroup() %>% 
+    as_tibble()
+
+
+
+
 #NPI = 0% ####
 orv_npi_simulation_npi0 <- orv_npi_control_config_table %>% 
     filter(npi_intensity == 0) %>% #npi levels of 0%
@@ -54,7 +75,7 @@ orv_npi_simulation_npi20 <- orv_npi_control_config_table %>%
     as_tibble()
 
 #NPI = 50% ####
-orv_npi_simulation_npi20 <- orv_npi_control_config_table %>% 
+orv_npi_simulation_npi50 <- orv_npi_control_config_table %>% 
     filter(npi_intensity == 0.5) %>% #npi levels of 0%, 10%, and 50%
     rowwise() %>% 
     do({with(., 
