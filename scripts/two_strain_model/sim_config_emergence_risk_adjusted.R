0e5e172c151e65f14487276bb0a66d8497cf578f
James Azam <jamesazam@sun.ac.za>
2021-10-25 02:49:46 +0200

    Use a finer resolution for vax coverage and speed and reduce the resolution of the npi levels

--- scripts/two_strain_model/sim_config_emergence_risk_adjusted.R
+++ scripts/two_strain_model/sim_config_emergence_risk_adjusted.R
@@ -16,7 +16,7 @@ source('./scripts/two_strain_model/two_strain_model.R')
 #Simple case: No NPI 
 npi_start <- 1
 npi_duration <- max_time - npi_start
-npi_intensity <- seq(0, 1, by = 0.1) #Five levels of npi intensity (could correspond to the five stages in South Africa, for e.g)
+npi_intensity <- seq(0, 1, by = 0.25) #Five levels of npi intensity (could correspond to the five stages in South Africa, for e.g)
 
 
 # Vaccination ====
@@ -27,8 +27,8 @@ npi_intensity <- seq(0, 1, by = 0.1) #Five levels of npi intensity (could corres
 
 # Simple case: vaccination starts on day 1 and can achieve 100% coverage but at different rates
 vax_start <- 1
-vax_cov <- seq(0.1, 1, by = 0.1) #various levels of coverage
-vax_speed_scenarios <- seq(1, 10, by = 1) #how many times faster than the daily vaccination rate
+vax_cov <- seq(0.1, 1, by = 0.025) #various levels of coverage
+vax_speed_scenarios <- seq(1, 10, by = 0.25) #how many times faster than the daily vaccination rate
 #daily_vax_rate <- as.vector(sapply(as.list(vax_cov/(max_time - vax_start)), function(x) {x*vax_speed_scenarios})) #daily rate of achieving the same coverage by the end of the period
 
 #Find the time it will take to achieve the assumed coverage with the given rates
@@ -74,14 +74,6 @@ orv_npi_control_config_table <- campaign_controls_scenarios_df %>%
            npi_duration = npi_duration
            )
 
-#Vaccination only intervention
-vax_only_config_table <- orv_npi_control_config_table %>% 
-    filter(npi_intensity == 0)
 
-
-
-#Baseline; no variant emerges and only wild type prevails
-baseline_params <- orv_npi_control_config_table %>% 
-    filter(variant_emergence_day == max_time)
 
 
