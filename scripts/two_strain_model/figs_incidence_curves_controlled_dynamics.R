80871965f390316df6a773731da473f965203b3d
James Azam <jamesazam@sun.ac.za>
2021-10-28 16:14:48 +0200

    First attempt to make incidence curves.

--- scripts/two_strain_model/figs_incidence_curves_controlled_dynamics.R
+++ scripts/two_strain_model/figs_incidence_curves_controlled_dynamics.R
@@ -16,9 +16,23 @@ controlled_epidemic_dynamics <- readRDS('./model_output/control_dynamics_case_st
 #Rescale the population proportions to total sizes
 controlled_epidemic_dynamics_rescaled <- controlled_epidemic_dynamics %>%
     select(!starts_with(c('S', 'R', 'V', 'K'), ignore.case = FALSE)) %>% 
-    mutate(time = time, 
+    mutate(time = time,
+           variant_emergence_day = variant_emergence_day,
               incidence = Iw + Iwm + Im + Imw,
-              variant_emerges = ifelse(variant_emergence_day < max_time, 'yes', 'no'), 
               .keep = 'unused'
               ) %>% 
     mutate(across(.cols = incidence, .fns = ~ .x*target_pop))
+
+
+#The incidence curves
+incidence_curves <- ggplot(data = controlled_epidemic_dynamics_rescaled) + 
+    geom_line(aes(x = time, 
+                  y = incidence,
+                  color = variant_emergence_day
+                  ), 
+              size = 0.1
+              ) + 
+    facet_wrap(~ as.factor(vax_speed))
+
+
+print(incidence_curves)
