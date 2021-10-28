9e7da7fba2c513fc005943f5dfdc87b2fff1a119
James Azam <jamesazam@sun.ac.za>
2021-10-28 16:41:52 +0200

    Run and grab the dynamics for the unmitigated epidemic

--- scripts/two_strain_model/simulations_uncontrolled_epidemic.R
+++ scripts/two_strain_model/simulations_uncontrolled_epidemic.R
@@ -8,13 +8,18 @@ source('./scripts/two_strain_model/sim_config_global_params.R')
 source('./scripts/two_strain_model/sim_config_uncontrolled_epidemic.R')
 
 # Simulations ====
-no_control_epidemic_dynamics <- variant_emergence_times %>% 
+
+variant_emergence_days <- seq(1, max_time, by = 1) #variant can emerge any day of the epidemic
+
+
+no_control_epidemic_dynamics <- variant_emergence_days %>% 
     purrr::map_df(function(x){simulate_raw_dynamics(pop_inits = pop_inits, 
                                              dynamics_parms = dynamics_params,
                                              control_parms = cbind(no_control_parms_df, data.frame(variant_emergence_day = x)),
                                              max_time = max_time, 
                                              dt = eval_times,
                                              events_table = event_df,
+                                             get_summaries = FALSE,
                                              browse = FALSE)}
                   )
 
