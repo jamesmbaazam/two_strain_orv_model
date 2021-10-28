89def2aaf8ead60a32aa7474e1ae802033e077c4
James Azam <jamesazam@sun.ac.za>
2021-10-28 16:41:30 +0200

    Add some missing control parameters

--- scripts/two_strain_model/sim_config_uncontrolled_epidemic.R
+++ scripts/two_strain_model/sim_config_uncontrolled_epidemic.R
@@ -8,7 +8,8 @@ source('./scripts/two_strain_model/sim_config_global_params.R')
 
 
 # control parameters (turned off for uncontrolled epidemic)
-no_control_parms_df <- data.frame(npi_intensity = 0, vax_coverage = 0, 
-                                  vax_start = max_time, campaign_duration = 0, 
+no_control_parms_df <- data.frame(vax_coverage = 0, vax_rate = 0, 
+                                  vax_speed = 0, vax_start = max_time, 
+                                  campaign_duration = 0, npi_intensity = 0, 
                                   npi_start = max_time, npi_duration = 0
-)
+                                  )
