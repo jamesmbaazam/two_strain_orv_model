6e6e0aa03d286258d1ad79b4a24121b314152637
James Azam <jamesazam@sun.ac.za>
2021-11-18 22:48:41 +0200

    Source the two strain model to use some needed functions

--- scripts/two_strain_model/figs_uncontrolled_epidemic.R
+++ scripts/two_strain_model/figs_uncontrolled_epidemic.R
@@ -6,6 +6,7 @@ library(tidyverse)
 #helper scripts
 source('./scripts/two_strain_model/sim_config_global_params.R')
 source('./scripts/two_strain_model/sim_config_uncontrolled_epidemic.R')
+source('./scripts/two_strain_model/two_strain_model.R')
 
 #Read the model output
 no_control_epidemic_dynamics <- readRDS('./model_output/uncontrolled_epidemic_dynamics.rds')
