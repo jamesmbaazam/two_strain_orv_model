9bfc1d050c8e7dd6900128bc99c24aad12311d4f
James Azam <jamesazam@sun.ac.za>
2022-01-23 22:37:57 +0200

    Comment out redundant code

--- scripts/two_strain_model/simulations_controlled_epidemic.R
+++ scripts/two_strain_model/simulations_controlled_epidemic.R
@@ -15,7 +15,7 @@ source('./scripts/two_strain_model/sim_config_emergence_risk_adjusted.R')
 
 #Simulations with npi and vaccination ====
 #All scenarios
-orv_npi_simulation_all_scenarios <- orv_npi_control_config_table %>% 
+orv_npi_simulation_all_scenarios <- simulation_config_table %>% 
     rowwise() %>% 
     do({with(., 
              simulate_raw_dynamics(pop_inits = pop_inits, 
@@ -36,69 +36,69 @@ orv_npi_simulation_all_scenarios <- orv_npi_control_config_table %>%
 
 
 #NPI = 0% ####
-orv_npi_simulation_npi0 <- orv_npi_control_config_table %>% 
-    filter(npi_intensity == 0) %>% #npi levels of 0%
-    rowwise() %>% 
-    do({with(., 
-             simulate_raw_dynamics(pop_inits = pop_inits, 
-                            dynamics_parms = dynamics_params,
-                            control_parms = .,
-                            max_time = max_time, 
-                            dt = eval_times,
-                            events_table = event_df,
-                            get_summaries = TRUE,
-                            browse = F
-             )
-    )
-    }) %>% 
-    ungroup() %>% 
-    as_tibble()
-
-
-#NPI = 20% ####
-orv_npi_simulation_npi20 <- orv_npi_control_config_table %>% 
-    filter(npi_intensity == 0.2) %>% #npi levels of 0%, 10%, and 50%
-    rowwise() %>% 
-    do({with(., 
-             simulate_raw_dynamics(pop_inits = pop_inits, 
-                                   dynamics_parms = dynamics_params,
-                                   control_parms = .,
-                                   max_time = max_time, 
-                                   dt = eval_times,
-                                   events_table = event_df,
-                                   get_summaries = TRUE,
-                                   browse = F
-             )
-    )
-    }) %>% 
-    ungroup() %>% 
-    as_tibble()
-
-#NPI = 50% ####
-orv_npi_simulation_npi50 <- orv_npi_control_config_table %>% 
-    filter(npi_intensity == 0.5) %>% #npi levels of 0%, 10%, and 50%
-    rowwise() %>% 
-    do({with(., 
-             simulate_raw_dynamics(pop_inits = pop_inits, 
-                                   dynamics_parms = dynamics_params,
-                                   control_parms = .,
-                                   max_time = max_time, 
-                                   dt = eval_times,
-                                   events_table = event_df,
-                                   get_summaries = TRUE,
-                                   browse = F
-             )
-    )
-    }) %>% 
-    ungroup() %>% 
-    as_tibble()
-
-
-
-#save the simulations
-saveRDS(object = orv_full_simulation, file = './model_output/controlled_epidemic_dynamics_sequential_run_npi0.rds')
-saveRDS(object = orv_full_simulation, file = './model_output/controlled_epidemic_dynamics_sequential_run_npi20.rds')
-saveRDS(object = orv_full_simulation, file = './model_output/controlled_epidemic_dynamics_sequential_run_npi50.rds')
+# orv_npi_simulation_npi0 <- orv_npi_control_config_table %>% 
+#     filter(npi_intensity == 0) %>% #npi levels of 0%
+#     rowwise() %>% 
+#     do({with(., 
+#              simulate_raw_dynamics(pop_inits = pop_inits, 
+#                             dynamics_parms = dynamics_params,
+#                             control_parms = .,
+#                             max_time = max_time, 
+#                             dt = eval_times,
+#                             events_table = event_df,
+#                             get_summaries = TRUE,
+#                             browse = F
+#              )
+#     )
+#     }) %>% 
+#     ungroup() %>% 
+#     as_tibble()
+# 
+# 
+# #NPI = 20% ####
+# orv_npi_simulation_npi20 <- orv_npi_control_config_table %>% 
+#     filter(npi_intensity == 0.2) %>% #npi levels of 0%, 10%, and 50%
+#     rowwise() %>% 
+#     do({with(., 
+#              simulate_raw_dynamics(pop_inits = pop_inits, 
+#                                    dynamics_parms = dynamics_params,
+#                                    control_parms = .,
+#                                    max_time = max_time, 
+#                                    dt = eval_times,
+#                                    events_table = event_df,
+#                                    get_summaries = TRUE,
+#                                    browse = F
+#              )
+#     )
+#     }) %>% 
+#     ungroup() %>% 
+#     as_tibble()
+# 
+# #NPI = 50% ####
+# orv_npi_simulation_npi50 <- orv_npi_control_config_table %>% 
+#     filter(npi_intensity == 0.5) %>% #npi levels of 0%, 10%, and 50%
+#     rowwise() %>% 
+#     do({with(., 
+#              simulate_raw_dynamics(pop_inits = pop_inits, 
+#                                    dynamics_parms = dynamics_params,
+#                                    control_parms = .,
+#                                    max_time = max_time, 
+#                                    dt = eval_times,
+#                                    events_table = event_df,
+#                                    get_summaries = TRUE,
+#                                    browse = F
+#              )
+#     )
+#     }) %>% 
+#     ungroup() %>% 
+#     as_tibble()
+# 
+# 
+# 
+# #save the simulations
+# saveRDS(object = orv_full_simulation, file = './model_output/controlled_epidemic_dynamics_sequential_run_npi0.rds')
+# saveRDS(object = orv_full_simulation, file = './model_output/controlled_epidemic_dynamics_sequential_run_npi20.rds')
+# saveRDS(object = orv_full_simulation, file = './model_output/controlled_epidemic_dynamics_sequential_run_npi50.rds')
 
 beep(sound = 3)
 
