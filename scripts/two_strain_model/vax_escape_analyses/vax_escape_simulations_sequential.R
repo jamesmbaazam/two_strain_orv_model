1c461c74b1b5ddf48330fc76ee15607eeab33ee4
James Azam <jamesazam@sun.ac.za>
2022-01-27 20:45:26 +0200

    Source the right script and function names

--- scripts/two_strain_model/vax_escape_analyses/vax_escape_simulations_sequential.R
+++ scripts/two_strain_model/vax_escape_analyses/vax_escape_simulations_sequential.R
@@ -6,10 +6,11 @@ library(tidyverse)
 library(beepr)
 
 # Helper scripts ----
-source('./scripts/two_strain_model/two_strain_model.R')
+source('./scripts/two_strain_model/two_strain_model_functions.R')
+source('./scripts/two_strain_model/simulation_functions.R')
 source('./scripts/two_strain_model/vax_escape_analyses/vax_escape_sim_global_params.R')
 source('./scripts/two_strain_model/vax_escape_analyses/vax_escape_sim_table_setup.R')
-source('./scripts/two_strain_model/simulation_functions.R')
+
 
 
 # Controlled epidemic ----
@@ -21,14 +22,14 @@ source('./scripts/two_strain_model/simulation_functions.R')
 vax_escape_perfect_vax_simulations <- vax_escape_mod_sim_table %>% 
     rowwise() %>% 
     do({with(., 
-             simulate_vax_escape_dynamics(pop_inits = pop_inits, 
+             simulate_dynamics_vax_escape_ts_model(pop_inits = pop_inits, 
                                    dynamics_parms = dynamics_params,
                                    control_parms = .,
                                    max_time = max_time, 
                                    dt = eval_times,
                                    events_table = event_df,
                                    get_summaries = TRUE, #only extract the summaries 
-                                   browse = F
+                                   browse = FALSE
              )
     )
     }) %>% 
@@ -37,6 +38,6 @@ vax_escape_perfect_vax_simulations <- vax_escape_mod_sim_table %>%
 
 
 #save the results
-saveRDS(object = orv_full_simulation, file = './model_output/vax_escape_perfect_vax_summaries.rds')
+saveRDS(object = vax_escape_perfect_vax_simulations, file = './model_output/vax_escape_perfect_efficacy_summaries.rds')
 
 beep(sound = 3)
