0531bf130dd34a648f5750354f4193491ba8a6a9
James Azam <jamesazam@sun.ac.za>
2022-01-28 22:25:50 +0200

    Cleaned up  the script to plot the cross protection sensitivity analyses

--- scripts/two_strain_model/vax_escape_analyses/sensitivity_analyses/cross_protection/figs_cross_protection_sensitivity.R
+++ scripts/two_strain_model/vax_escape_analyses/sensitivity_analyses/cross_protection/figs_cross_protection_sensitivity.R
@@ -5,40 +5,36 @@ library(patchwork)
 library(mdthemes)
 
 #helper scripts
-source('./scripts/two_strain_model/two_strain_model.R')
-source('./scripts/two_strain_model/sim_config_global_params.R')
-source('./scripts/two_strain_model/sim_config_emergence_risk_adjusted.R')
+source('./scripts/two_strain_model/two_strain_model_functions.R')
+source('./scripts/two_strain_model/vax_escape_analyses/sensitivity_analyses/global_inputs/global_params.R')
+source('./scripts/two_strain_model/vax_escape_analyses/sensitivity_analyses/global_inputs/intervention_params_table_setup_global.R')
 
 #plot paths for thesis and git
 git_plot_path <- './figures/'
 thesis_plot_path <- "C:/Users/JAMESAZAM/Dropbox/My Academic Repository/_SACEMA/Academic/_PhD/__PhD_Thesis/_Thesis_LaTeX/figs/chapter_5/"
 
 #Load the model output
-full_cross_protection_model_summaries <- readRDS('./model_output/orv_npi_all_scenarios_dynamics_parallel.rds') %>% 
-    mutate(R0m = R0_m, 
-           sigma_w = dynamics_params$sigma_w,
-           sigma_m = dynamics_params$sigma_m,
-           vax_efficacy_w = 1,
-           vax_efficacy_m = 1
-           )
-    
-partial_cross_protection_model_summaries <- readRDS('./model_output/sensitivity_analyses/cross_protection/model_summaries_partial_cross_protection.rds')
+
+# Original analysis
+#' The baseline analysis assumes perfect vaccine efficacy against the wild-type 
+#' and variant and perfect cross protection between the strains. The variant is
+#' assumed to be 30% more transmissible than the wild-type (R0w = 1.3R0m)
+baseline_analysis_results <- readRDS('./model_output/vax_escape_analyses/vax_escape_perfect_efficacy_summaries_vR0_30.rds')
+
+#Cross protection sensitivity analysis results
+cp_sensitivity_results <- readRDS('./model_output/sensitivity_analyses/cross_protection/cp_sensitivity_summaries.rds')
 
 
 #combine the two model outputs
-cross_protection_sensitivity_df <- bind_rows(full_cross_protection_model_summaries, 
-                                             partial_cross_protection_model_summaries
-                                             ) %>% 
-    select(-contains('efficacy'))
+cp_sensitivity_analysis_df <- bind_rows(baseline_analysis_results, cp_sensitivity_results) 
 
 
 
 #Rescale the population proportions to total sizes
-cross_protection_sensitivity_pop_rescale <- cross_protection_sensitivity_df %>%
+cp_sensitivity_analysis_pop_rescaled <- cp_sensitivity_analysis_df %>%
     mutate(across(.cols = c(total_cases, peak_cases, total_vaccinated), 
-                  .fns = ~ .x*target_pop #rescale the population proportions to total sizes
-    )
-    ) %>% 
+                  .fns = ~ .x*target_pop)
+           ) %>%  #rescale the population proportions to total sizes
     select(-c(npi_duration, total_vaccinated))
 
 
@@ -47,11 +43,10 @@ cross_protection_sensitivity_pop_rescale <- cross_protection_sensitivity_df %>%
 
 #' Towards the outbreak size isoclines
 
-outbreak_size_isocline_df <- cross_protection_sensitivity_pop_rescale %>% 
-    filter(variant_emergence_day %in% c(1, 61, 121, 151, max_time), 
-           npi_intensity %in% c(0.0, 0.1, 0.2, 0.3), 
+outbreak_size_cp_isocline_df <- cp_sensitivity_analysis_pop_rescaled %>% 
+    filter(npi_intensity %in% c(0.0, 0.1, 0.2, 0.3), 
            total_cases <= 1000
-    ) %>% 
+           ) %>% 
     mutate(variant_emergence_day = as_factor(variant_emergence_day)) %>% 
     group_by(variant_emergence_day, vax_coverage, npi_intensity) %>% 
     mutate(min_speed = min(vax_speed)) %>% 
@@ -59,12 +54,12 @@ outbreak_size_isocline_df <- cross_protection_sensitivity_pop_rescale %>%
 
 # Isoclines ----
 # Outbreak size ====
-outbreak_size_isocline_cp_sensitivity <- ggplot(outbreak_size_isocline_df, 
+outbreak_size_cp_isocline_sensitivity <- ggplot(outbreak_size_cp_isocline_df, 
                                  aes(x = vax_coverage, 
                                      y = min_speed, 
                                      color = variant_emergence_day
                                  )) + 
-    geom_line(aes(linetype = factor(sigma_w)), 
+    geom_line(aes(linetype = factor(cross_protection_w)), 
               size = 1, 
               show.legend = TRUE
               ) + 
@@ -81,18 +76,18 @@ outbreak_size_isocline_cp_sensitivity <- ggplot(outbreak_size_isocline_df,
     theme(strip.text.x = element_text(size = 12, face = 'bold'), legend.position = 'bottom') 
 
 
-print(outbreak_size_isocline_cp_sensitivity)
+print(outbreak_size_cp_isocline_sensitivity)
 
 #Save the files 
-ggsave(outbreak_size_isocline_cp_sensitivity,
-       filename = 'outbreak_size_isocline_cp_sensitivity.png',
+ggsave(outbreak_size_cp_isocline_sensitivity,
+       filename = 'outbreak_size_cp_isocline_sensitivity.png',
        path = git_plot_path,
        width = 23.76,
        height = 17.86,
        units = 'cm')
 
-ggsave(outbreak_size_isocline_cp_sensitivity,
-       filename = 'outbreak_size_isocline_cp_sensitivity.eps',
+ggsave(outbreak_size_cp_isocline_sensitivity,
+       filename = 'outbreak_size_cp_isocline_sensitivity.eps',
        path = thesis_plot_path,
        width = 23.76,
        height = 17.86,
@@ -102,17 +97,15 @@ ggsave(outbreak_size_isocline_cp_sensitivity,
 
 # Peak prevalence ====
 
-peak_prevalence_isocline_df <- controlled_epidemic_rescaled %>% 
-    filter(variant_emergence_day %in% c(1, 61, 121, 151, max_time), 
-           npi_intensity %in% c(0.0, 0.1, 0.2, 0.3), 
-           peak_cases <= 300
-    ) %>% 
+peak_prevalence_cp_isocline_df <- cp_sensitivity_analysis_pop_rescaled %>% 
+    filter(npi_intensity %in% c(0.0, 0.1, 0.2, 0.3), 
+           peak_cases <= 300) %>% 
     group_by(variant_emergence_day, vax_coverage, npi_intensity) %>% 
     mutate(variant_emergence_day = as_factor(variant_emergence_day)) %>% 
     mutate(min_speed = min(vax_speed)) %>% 
     ungroup()
 
-peak_prevalence_isocline <- ggplot(peak_prevalence_isocline_df, 
+peak_prevalence_cp_isocline <- ggplot(peak_prevalence_cp_isocline_df, 
                                    aes(x = vax_coverage, 
                                        y = min_speed, 
                                        color = variant_emergence_day
@@ -131,19 +124,19 @@ peak_prevalence_isocline <- ggplot(peak_prevalence_isocline_df,
     theme(strip.text.x = element_text(size = 12, face = 'bold'), legend.position = 'bottom') 
 
 
-print(peak_prevalence_isocline)
+print(peak_prevalence_cp_isocline)
 
 
 #Save the files 
-ggsave(peak_prevalence_isocline,
-       filename = 'peak_prevalence_isocline.png',
+ggsave(peak_prevalence_cp_isocline,
+       filename = 'peak_prevalence_cp_isocline.png',
        path = git_plot_path,
        width = 23.76,
        height = 17.86,
        units = 'cm')
 
-ggsave(peak_prevalence_isocline,
-       filename = 'peak_prevalence_isocline.eps',
+ggsave(peak_prevalence_cp_isocline,
+       filename = 'peak_prevalence_cp_isocline.eps',
        path = thesis_plot_path,
        width = 23.76,
        height = 17.86,
