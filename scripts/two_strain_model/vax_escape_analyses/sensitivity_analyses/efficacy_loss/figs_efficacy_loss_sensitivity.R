855a885fc4d93c761db3b1babdc63ed77feee9aa
James Azam <jamesazam@sun.ac.za>
2022-01-29 21:33:45 +0200

    Script to plot efficacy loss sensitivity analyses

---
+++ scripts/two_strain_model/vax_escape_analyses/sensitivity_analyses/efficacy_loss/figs_efficacy_loss_sensitivity.R
@@ -0,0 +1,159 @@
+#Packages ----
+library(tidyverse)
+library(scales)
+library(patchwork)
+library(mdthemes)
+
+#helper scripts
+source('./scripts/two_strain_model/two_strain_model_functions.R')
+source('./scripts/two_strain_model/vax_escape_analyses/sensitivity_analyses/global_inputs/global_params.R')
+source('./scripts/two_strain_model/vax_escape_analyses/sensitivity_analyses/global_inputs/intervention_params_table_setup_global.R')
+
+#plot paths for thesis and git
+git_plot_path <- './figures/'
+thesis_plot_path <- "C:/Users/JAMESAZAM/Dropbox/My Academic Repository/_SACEMA/Academic/_PhD/__PhD_Thesis/_Thesis_LaTeX/figs/chapter_5/"
+
+#Load the model output
+
+# Original analysis
+#' The baseline analysis assumes perfect vaccine efficacy against the wild-type 
+#' and variant and perfect cross protection between the strains. The variant is
+#' assumed to be 30% more transmissible than the wild-type (R0w = 1.3R0m)
+baseline_analysis_results <- readRDS('./model_output/sensitivity_analyses/baseline_analysis_vR0_30_percent/vR0_30_percent_baseline_analysis_summaries.rds') %>% 
+    rename(peak_prevalence = peak_cases)
+
+#Cross protection sensitivity analysis results
+efficacy_loss_sensitivity_analysis_results <- readRDS('./model_output/sensitivity_analyses/efficacy_loss/efficacy_loss_sensitivity_analysis_summaries.rds')
+
+
+#combine the two model outputs
+efficacy_loss_sensitivity_analysis_all_results <- bind_rows(baseline_analysis_results, efficacy_loss_sensitivity_analysis_results) 
+
+#Remove redundant columns
+efficacy_loss_sensitivity_analysis_df <- efficacy_loss_sensitivity_analysis_all_results %>% 
+    select(-c(vax_rate, 
+              npi_duration, 
+              starts_with('cross_protection'), 
+              R0m,
+              total_vaccinated
+              )
+           )
+
+#Rescale the population proportions to total sizes
+efficacy_loss_sensitivity_analysis_pop_rescaled <- efficacy_loss_sensitivity_analysis_df %>%
+    mutate(across(.cols = c(total_cases, peak_prevalence), 
+                  .fns = ~ .x*target_pop
+                  ),
+           variant_emergence_day = as_factor(variant_emergence_day), 
+           vax_efficacy_w = as_factor(vax_efficacy_w),
+           vax_efficacy_m = as_factor(vax_efficacy_m)
+           ) 
+
+#' Determine the subset of scenarios that meet the threshold outbreak size
+#' and the minimum speed required 
+outbreak_size_efficacy_loss_isocline_df <- efficacy_loss_sensitivity_analysis_pop_rescaled %>% 
+    filter(variant_emergence_day %in% c(1, 61, 121, 151, max_time), 
+           npi_intensity %in% c(0.0, 0.1, 0.2, 0.3), 
+           total_cases <= 1000 #only keep scenarios with 1000 total cases or less
+           ) %>% 
+    group_by(variant_emergence_day, vax_coverage, npi_intensity, vax_efficacy_m) %>% 
+    mutate(min_speed = min(vax_speed)) %>% 
+    ungroup()
+
+# Isoclines ----
+# Outbreak size ====
+outbreak_size_efficacy_loss_isocline_sensitivity <- ggplot(outbreak_size_efficacy_loss_isocline_df %>% 
+                                                               filter(vax_efficacy_m %in% c(1, 0.6)), 
+                                 aes(x = vax_coverage, 
+                                     y = min_speed, 
+                                     color = variant_emergence_day
+                                 )) + 
+    geom_line(aes(linetype = vax_efficacy_m), 
+              size = 1, 
+              show.legend = TRUE
+              ) + 
+    scale_x_continuous(labels = percent_format(), breaks = seq(0.30, 1, 0.1)) +
+    scale_y_continuous(breaks = seq(1, 10, 1), labels = seq(1, 10, 1)) +
+    scale_color_viridis_d(option = 'viridis') +
+    labs(#title = paste('Cumulative cases threshold <= 1000 at various NPI intensity levels'), 
+        x = 'Vaccination coverage', 
+        y = 'Vaccination speed', 
+        color = 'Variant emergence day',
+        linetype = 'Vaccine efficacy against variant'
+    ) +
+    facet_wrap('npi_intensity', labeller = 'label_both') +
+    theme_bw(base_size = 14) +
+    theme(strip.text.x = element_text(size = 12, face = 'bold'), legend.position = 'bottom') 
+
+
+print(outbreak_size_efficacy_loss_isocline_sensitivity)
+
+#Save the files 
+ggsave(outbreak_size_efficacy_loss_isocline_sensitivity,
+       filename = 'outbreak_size_efficacy_loss_isocline_sensitivity.png',
+       path = git_plot_path,
+       width = 23.76,
+       height = 17.86,
+       units = 'cm')
+
+ggsave(outbreak_size_efficacy_loss_isocline_sensitivity,
+       filename = 'outbreak_size_efficacy_loss_isocline_sensitivity.eps',
+       path = thesis_plot_path,
+       width = 23.76,
+       height = 17.86,
+       units = 'cm')
+
+
+
+# Peak prevalence ====
+
+peak_prevalence_efficacy_loss_isocline_df <- efficacy_loss_sensitivity_analysis_pop_rescaled %>% 
+    filter(variant_emergence_day %in% c(1, 61, 121, 151, max_time),
+           npi_intensity %in% c(0.0, 0.1, 0.2, 0.3), 
+           peak_prevalence <= 300) %>% 
+    group_by(variant_emergence_day, vax_coverage, npi_intensity, cross_protection_w) %>% 
+    mutate(min_speed = min(vax_speed)) %>% 
+    ungroup()
+
+peak_prevalence_efficacy_loss_isocline <- ggplot(peak_prevalence_efficacy_loss_isocline_df, 
+                                   aes(x = vax_coverage, 
+                                       y = min_speed, 
+                                       color = variant_emergence_day
+                                   )) + 
+    geom_line(aes(linetype = cross_protection_w), size = 1, show.legend = TRUE) + 
+    scale_x_continuous(labels = percent_format(), breaks = seq(0.30, 1, 0.1)) +
+    scale_y_continuous(breaks = seq(1, 10, 1), labels = seq(1, 10, 1)) +
+    scale_color_viridis_d(option = 'viridis') +
+    labs(#title = paste('Peak prevalence <= 300 at various NPI intensity levels'), 
+        x = 'Vaccination coverage', 
+        y = 'Vaccination speed', 
+        color = 'Variant emergence day',
+        linetype = 'Cross protection'
+    ) +
+    facet_wrap('npi_intensity', labeller = 'label_both') +
+    theme_bw(base_size = 14) +
+    theme(strip.text.x = element_text(size = 12, face = 'bold'), legend.position = 'bottom') 
+
+
+print(peak_prevalence_efficacy_loss_isocline)
+
+
+#Save the files 
+ggsave(peak_prevalence_efficacy_loss_isocline,
+       filename = 'peak_prevalence_efficacy_loss_isocline.png',
+       path = git_plot_path,
+       width = 23.76,
+       height = 17.86,
+       units = 'cm')
+
+ggsave(peak_prevalence_efficacy_loss_isocline,
+       filename = 'peak_prevalence_efficacy_loss_isocline.eps',
+       path = thesis_plot_path,
+       width = 23.76,
+       height = 17.86,
+       units = 'cm')
+
+
+
+
+
