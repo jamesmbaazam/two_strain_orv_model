56d7a97ee391328659c3a81b036531d45d2ce00f
James Azam <jamesazam@sun.ac.za>
2022-02-01 15:49:59 +0200

    Fomatted the code for readability

--- scripts/two_strain_model/sim_config_uncontrolled_epidemic.R
+++ scripts/two_strain_model/sim_config_uncontrolled_epidemic.R
@@ -1,7 +1,11 @@
 
 # control parameters (turned off for uncontrolled epidemic)
-no_control_parms_df <- data.frame(vax_coverage = 0, vax_rate = 0, 
-                                  vax_speed = 0, vax_start = max_time, 
-                                  campaign_duration = 0, npi_intensity = 0, 
-                                  npi_start = max_time, npi_duration = 0
+no_control_parms_df <- data.frame(vax_coverage = 0, 
+                                  vax_rate = 0, 
+                                  vax_speed = 0, 
+                                  vax_start = max_time, 
+                                  campaign_duration = 0, 
+                                  npi_intensity = 0.3, 
+                                  npi_start = max_time, 
+                                  npi_duration = 0
                                   )
